7a95f773550ef567b3d7d4729129707e6d172187
Lorenzo De Luca <lorenzo@deluca.pro>
2026-07-06 04:24:50 +0200

    Add privacy notice to index.php

---
+++ privacy/index.php
@@ -0,0 +1,3 @@
+Questo sito non usa cookies se non funzionali.
+<br><br>
+Alcuni dei dati(ad esempio ip, user agent, informazioni sul dispositivo) degli utenti che visitano questo sito potrebbero essere usati a scopi analitici e per il corretto funzionamento del sito
