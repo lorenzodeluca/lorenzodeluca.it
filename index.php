4b1e881ff183ded54c2088fcac34ecd5dcc282d2
Lorenzo De Luca <lorenzo@deluca.pro>
2026-07-06 04:24:07 +0200

    Create index.php for personal portfolio website

---
+++ index.php
@@ -0,0 +1,90 @@
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+  <title>Lorenzo De Luca</title>
+  <style>
+  .text{
+   color: white;
+  }
+  body{
+   background-color: black;
+  }
+  .title{
+   margin-bottom: 0;
+  }
+  .info{
+   font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+   margin-left: 10%;
+   margin-top: 5%;
+   color: white;
+  }
+  .contact{
+   padding-top: 100px;
+ }
+  .icon{
+   color: white;
+   padding: 10px;
+  }
+  a{
+   color: white;
+   text-decoration: none;
+  }
+ .footer{
+   position:fixed; 
+   width:100%;
+   bottom: 0;
+   text-align: center;
+  }
+ .copyrg{
+   font-size: 20px;
+   font-family: 'Brush Script MT';
+ }
+  </style>
+ </head>
+  <body>
+   <!-- <b><i><a href="https://deluca.pro/it">versione in italiano</a></i></b>  --!>
+   <div class="info">
+    <div class="personal">
+     <b><h1 class="title">Lorenzo De Luca</h1></b><br>
+     <i class="fa-solid fa-location-dot icon"></i>Venice<br>
+     <i class="fa-solid fa-terminal icon"></i>Full Stack Developer<br>
+     <i class="fa-solid fa-laptop icon"></i>BSc in Computer Engineering - University of Padua<br>
+     <i class="fa-solid fa-laptop icon"></i>MSc in Computer Engineering - University of Bologna<br>
+     <i class="fa-solid fa-heart-pulse icon"></i>Lifeguard - Professional rescuer
+    </div>
+    <div class="contact">
+     Contact informations(click on them):<br>
+     <i class="fa-solid fa-at icon"></i><a href="mailto:lorenzo@deluca.pro">lorenzo@deluca.pro</a><br>
+     <i class="fa-solid fa-at icon"></i><a href="mailto:venezia@lorenzodeluca.it">venezia@lorenzodeluca.it</a><br>
+     <i class="fa-brands fa-linkedin icon"></i><a href="https://www.linkedin.com/in/lorenzodeluca">Linkedin</a><br>
+     <i class="fa-brands fa-github icon"></i><a href="https://github.com/lorenzodeluca">Github</a><br>
+     <i class="fa-brands fa-gitlab icon"></i><a href="https://gitlab.com/lorenzodeluca">Gitlab</a><br>
+     <i class="fab fa-github icon"></i><a href="https://stackoverflow.com/users/9441578/lorenzo?tab=profile">stackoverflow</a><br>
+     <i class="fa-brands fa-redhat icon"></i><a href="https://www.hackthebox.com/home/users/profile/79745">HackTheBox</a>
+    </div>
+   <br><br>
+   <div class="hobbies">
+     <i class="fa-solid fa-film icon"></i><a href="https://www.imdb.com/user/ur145310474/?ref_=nv_usr_prof_2">Films? IMDB</a><br>
+     <i class="fa-solid fa-music icon"></i><a href="https://open.spotify.com/user/qvm9vnpgjiv4ygdslrjlmycj6">Music? pop, rap, hip-hop, rock, techno, jazz... here my spotify</a><br>
+     <i class="fa-solid fa-book icon"></i><a href="https://www.librarything.com/catalog/TheOfficialShocker">Books (from 2023)</a><br>
+   </div>
+  </div>
+<br><br> Some of my projects: Coming very soon...
+ <a href="/privacy">privacy policy</a>
+  <div class="footer text copyrg"><b><i class="fa fa-copyright" aria-hidden="true"></i>Lorenzo De Luca 2021 <i class="fa-solid fa-face-laugh-wink"></i></b></div>
+  </body>
+</html>
+
+<!--
+  heyyyy, you are not supposed to look here -_-
+  anyway, since u are here :)
+  coming soon(TODO):
+    -html5 tags
+    -improve css and move it to a different file
+    -improve graphic
+    -add past projects
+    -format text
+    -add some easter egg here and there
+-->
